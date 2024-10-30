049c0eda4076c023d5753ef5baccad31bc91af30
nıdaturkay <145764089+NdaTrky@users.noreply.github.com>
2024-10-30 20:31:00 +0300

    react-hook-form yüklendi

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import  {useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { api } from '@/api';
